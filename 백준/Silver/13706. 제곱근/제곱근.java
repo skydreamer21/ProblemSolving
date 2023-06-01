ebef4f6ac613e89c2fac2a55cc42f3a9fc486e6f
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2023-06-01 13:26:02 +0900

    [Silver IV] Title: 제곱근, Time: 164 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Silver/13706. 제곱근/제곱근.java
+++ 백준/Silver/13706. 제곱근/제곱근.java
@@ -8,11 +8,21 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         
         BigInteger N = new BigInteger(br.readLine());
-        sb.append(N.sqrt());
+        sb.append(sqrt(N));
     
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
+    }
+    
+    private static BigInteger sqrt(BigInteger n) {
+        BigInteger x = n;
+        BigInteger y = (x.add(new BigInteger("1"))).shiftRight(1);
+        while (y.compareTo(x) < 0) {
+            x = y;
+            y = x.add(n.divide(x)).shiftRight(1);
+        }
+        return x;
     }
 }

8ee3728f1757373503eb85ff1817ad6b78880a4e
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2023-06-01 15:05:59 +0900

    [Silver III] Title: N과 M (6), Time: 80 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] combination;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        combination = new int[M];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        combi(0, 0);
+    
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    private static void combi(int cnt, int start) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(combination[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = start; i < N; i++) {
+            combination[cnt] = arr[i];
+            combi(cnt + 1, i + 1);
+        }
+    }
+}

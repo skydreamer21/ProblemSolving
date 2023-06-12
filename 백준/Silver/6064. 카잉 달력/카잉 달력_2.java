13af1b4bce3f6ede86242eb54ab1e0f394275975
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2023-06-12 16:36:39 +0900

    solve: add another solution of Q6064

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력_2.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final int IMPOSSIBLE = -1;
+    
+    static int N, M, A, B;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            boolean findAns = false;
+            
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            
+            A = A == M ? 0 : A;
+            B = B == N ? 0 : B;
+            
+            if (N > M) {
+                int temp = M;
+                M = N;
+                N = temp;
+                
+                temp = A;
+                A = B;
+                B = temp;
+            }
+            
+            int lcm = lcm(M, N);
+            if (A == 0 && B == 0) {
+                sb.append(lcm).append("\n");
+                continue;
+            }
+            for (int i = A; i < lcm; i += M) {
+                if (i % N == B) {
+                    sb.append(i).append("\n");
+                    findAns = true;
+                    break;
+                }
+            }
+            
+            if (!findAns) {
+                sb.append(IMPOSSIBLE).append("\n");
+            }
+        }
+    
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    
+    private static int lcm(int a, int b) {
+        int gcd = gcd(a, b);
+        return a * b / gcd;
+    }
+}

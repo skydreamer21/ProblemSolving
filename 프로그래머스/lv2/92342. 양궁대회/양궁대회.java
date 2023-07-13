c8c397a39e72669cb06b38e48be5a9a12a85a2e1
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2023-07-13 20:30:58 +0900

    [level 2] Title: 양궁대회, Time: 0.42 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -1,63 +1,6 @@
 class Solution {
-    static int N;
-    static int[] info;
-    static int[] result;
-    static int[] answer;
-    static int maxDiff = 0;
-    
-    public int[] solution(int n, int[] i) {
-        N = n;
-        info = i;
-        result = new int[11];
-        answer = new int[11];
-        dfs(0, n, 0, 0);
-        
-        if (maxDiff == 0) {
-            return new int[]{-1};
-        } else {
-            return answer;
-        }
-    }
-    
-    public void dfs(int cnt, int arrow, int score1, int score2) {
-        // System.out.printf("[IN] cnt : %d, arrow : %d, score1 : %d, score2 : %d\n", cnt, arrow, score1, score2);
-        if (cnt == 11) {
-            int lastValue = result[10];
-            result[10] += arrow;
-            
-            if (score2 - score1 > maxDiff) {
-                // System.out.printf("maxDiff 갱신 (남은 화살 : %d)--> score1 : %d, score2 : %d\n",arrow, score1, score2);
-                maxDiff = score2 - score1;
-                System.arraycopy(result, 0, answer, 0, 11);
-            } else if (score2 - score1 == maxDiff) {
-                for (int i=10; i>=0; i--) {
-                    if (result[i] == answer[i]) continue;
-                    if (result[i] > answer[i]) {
-                        System.arraycopy(result, 0, answer, 0, 11);
-                        break;
-                    } else break;
-                }
-            }
-            // System.out.printf("[OUT - 더 진행 불가] cnt : %d, arrow : %d, score : %d\n", cnt, arrow, score);
-            
-            result[10] = lastValue;
-            return;
-        }
-        
-        int targetScore = 10 - cnt;
-        
-        // 1. 과녁에 대한 점수를 가져가는 경우
-        if (arrow > info[cnt]) { // 상대가 쏜 화살보다 더 많이 가지고 있어야 한다.
-            result[cnt] = info[cnt]+1;
-            dfs(cnt+1, arrow - info[cnt] - 1, score1, score2+targetScore);
-            result[cnt] = 0;
-        }
-        
-        // 2. 과녁에 대한 점수를 가져가지 않는 경우
-        dfs(cnt+1, arrow, 
-            info[cnt] > 0 ? score1 + targetScore : score1, 
-            score2);
-        
-        // System.out.printf("[OUT - 모든 경우 탐색] cnt : %d, arrow : %d, score : %d\n", cnt, arrow, score);
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+        return answer;
     }
 }

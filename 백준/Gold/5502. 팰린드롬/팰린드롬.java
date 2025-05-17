75aa596ca353a053c4db824f513bbd0a4e281f0d
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2025-05-17 13:02:48 +0900

    [Gold III] Title: 팰린드롬, Time: 212 ms, Memory: 110264 KB -BaekjoonHub

---
+++ 백준/Gold/5502. 팰린드롬/팰린드롬.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static char[] str;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        str = br.readLine().toCharArray();
+        dp = new int[N+1][N+1];
+        int min = N-1;
+        for (int i = 1; i<N; i++) {
+            int end = N - i;
+            for (int j=1; j<=end; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if (str[i-1] == str[N-j]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1] + 1);
+                }
+            }
+            int rowMin = Math.min(N - (2*dp[i][end]), N - 1 - (2*dp[i][end-1]));
+            min = Math.min(min, rowMin);
+        }
+
+        sb.append(min);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+}

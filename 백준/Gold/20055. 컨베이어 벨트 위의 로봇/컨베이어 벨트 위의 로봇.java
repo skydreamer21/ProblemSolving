d7afeff57c99cdd5b5f9ab344616e59d5471cc3c
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2025-06-11 22:22:29 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 216 ms, Memory: 21856 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -5,97 +5,118 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static Machine machine;
+    static int limit;
 
-    static class Machine {
-        int size;
-        int start;
-        int[] belt;
-        boolean[] hasRobot;
-        int zeroLimit;
-        int zeroCount;
+    static class Node {
+        int idx;
 
-        public Machine(int size, int zeroLimit) {
-            this.size = 2 * size;
-            start = 0;
-            belt = new int[this.size];
-            hasRobot = new boolean[this.size];
-            this.zeroLimit = zeroLimit;
-            zeroCount = 0;
+        public Node(int idx) {
+            this.idx = idx;
         }
 
-        public void run() {
-            rotate();
-            moveRobots();
-            load();
+        public int next(int mod) {
+            return (idx + mod -1) % mod;
         }
 
-        public boolean isRunnable() {
-            return zeroCount < zeroLimit;
-        }
-
-        public void rotate() {
-            start = start == 0 ? size - 1 : start - 1;
-            int end = getEnd();
-            if (hasRobot[end]) {
-                hasRobot[end] = false;
-            }
-        }
-
-        public void moveRobots() {
-            int end = getEnd();
-            for (int i = 1; i <= size/2 - 2; i++) { // 시작과 끝은 무조건 비어있는 상태임
-                int pos = getPos(end, -i);
-                // 해당 칸에 로봇이 없다면 pass
-                if (!hasRobot[pos]) continue;
-
-                // 다음 칸에 로봇을 올릴 수 없다면 pass
-                if (isNotAvailable(getPos(pos, 1))) continue;
-
-                // 이번 칸에 로봇이 있고 다음칸으로 옮길 수 있다면 옮긴다.
-                hasRobot[pos] = false;
-                int nextPos = getPos(pos, 1);
-                hasRobot[nextPos] = true;
-                belt[nextPos]--;
-
-                if (belt[nextPos] == 0) {
-                    zeroCount++;
-                }
-            }
-            hasRobot[end] = false;
-        }
-
-        public void load() {
-            if (hasRobot[start]) { // 나오면 안되는 상황
-                throw new IllegalArgumentException("[ERROR] 시작 위치에 로봇이 있습니다.");
-            }
-
-            if (belt[start] > 0) {
-                hasRobot[start] = true;
-                belt[start]--;
-                if (belt[start] == 0) {
-                    zeroCount++;
-                }
-            }
-        }
-
-        private boolean isNotAvailable(int idx) {
-            return hasRobot[idx] || belt[idx] == 0;
-        }
-
-        private int getEnd() {
-            return (start + size / 2 - 1) % size;
-        }
-
-        private int getPos(int idx, int dx) {
-            return (idx + size + dx) % size;
+        public void move(int to) {
+            this.idx = to;
         }
     }
-    
+
+    static class CV {
+        int N;
+        int[] hp;
+        boolean[] hasRobot;
+        int UP_START;
+        int DOWN_START;
+        Deque<Node> q;
+        int cnt = 0;
+
+
+        public CV(int[] hp) {
+            N = hp.length;
+            this.hp = hp;
+            hasRobot = new boolean[N];
+            UP_START = N -1;
+            DOWN_START = N/2;
+            q = new ArrayDeque<>();
+        }
+
+        public int operate() {
+            int step = 1;
+            while(moveCV(step)) {
+//                System.out.printf("[STEP %d] cnt: %d, result : %s\n", step, cnt, Arrays.toString(hp));
+                step++;
+//                if (step >= 20) break;
+            }
+            return step;
+        }
+
+        public boolean moveCV(int n) {
+
+            // 1. 현재 단계에서 올리는 칸과 내리는 칸 파악
+            int up = (UP_START + n) % N;
+            int down = (DOWN_START + n) % N;
+
+//            System.out.printf("[STEP %d], up:%d, down:%d\n", n, up, down);
+
+            // 2. 큐에 가장 앞서 있는 로봇이 내리는 칸에 있는지 검사
+            if (!q.isEmpty()) {
+//                System.out.printf("가장 앞선 로봇 : %d\n", q.peek().idx);
+
+            }
+            if (!q.isEmpty() && q.peek().idx == down) {
+//                System.out.printf("앞선 로봇이 %d에서 내림\n", q.peek().idx);
+                hasRobot[q.peek().idx] = false;
+                q.poll();
+            }
+
+            // 3. 큐의 요소들을 순회하면서 이동할 수 있는 로봇들을 이동시킨다.
+            for (Node node : q) {
+                int cur = node.idx;
+                int next = node.next(N);
+                if (hp[next] != 0 && !hasRobot[next]) {
+                    node.move(next);
+                    hp[next]--;
+                    if (hp[next] == 0) {
+                        cnt++;
+                        if (cnt == limit) return false;
+                    }
+                    hasRobot[cur] = false;
+                    if (next == down) {
+                        q.poll();
+                    } else {
+                        hasRobot[next] = true;
+                    }
+                }
+            }
+
+            // 4. 올리는 위치에 가능하면 로봇을 올린다.
+            if (hp[up] != 0) {
+                hp[up]--;
+                if (hp[up] == 0) {
+                    cnt++;
+                    if (cnt == limit) return false;
+                }
+                hasRobot[up] = true;
+                q.add(new Node(up));
+            }
+            for (int i = 0; i<N; i++) {
+                int p = (up + N -i)%N;
+//                System.out.printf("%d(%d) ", (p)%N, hasRobot[p] ? 1 : 0);
+            }
+//            System.out.println();
+            return true;
+        }
+
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -103,27 +124,22 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        machine = new Machine(N, M);
+        int N = Integer.parseInt(st.nextToken())*2;
+        limit = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < machine.size; i++) {
-            machine.belt[i] = Integer.parseInt(st.nextToken());
+        int[] hp = new int[N];
+        for (int i = 0; i<N; i++) {
+            hp[N-1-i] = Integer.parseInt(st.nextToken());
         }
 
-        int step = 0;
-        while (machine.isRunnable()) {
-            step++;
-            machine.run();
-        }
+        CV cv = new CV(hp);
 
-        sb.append(step);
-
+        sb.append(cv.operate());
 
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
+
 }

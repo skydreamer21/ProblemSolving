5264d874db959778714a67cb2175ea56cea2bb29
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2025-07-12 16:34:56 +0900

    [Gold IV] Title: 가르침, Time: 128 ms, Memory: 12968 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,100 +1,95 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
 import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Arrays;
 
 public class Main {
-    static int N, M;
-    static Set<Character>[] words;
-    static int max = 0;
-    static Set<Character> chosen;
-    static Set<Character> base;
-    static Character[] allCharacter = {
-            'b', 'd', 'e',
-            'f', 'g', 'h', 'j',
-            'k', 'l', 'm', 'o',
-            'p', 'q', 'r', 's',
-            'u', 'v', 'w', 'x', 'y',
-            'z'
-    };
+    static int N, K;
+    static int status = 0;
+    static int[] words;
+    static int maxReadableWord = 0;
+    static HashSet<Integer> learnedIdx = new HashSet<>(Arrays.asList(0, 2, 8, 13 ,19));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
+        // ******************** 입력 & 초기화 ********************
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        if (M < 5) {
-            sb.append(0);
-        } else {
-            words = new Set[N];
-            base = new HashSet<>();
-            base.add('a');
-            base.add('c');
-            base.add('i');
-            base.add('n');
-            base.add('t');
-            for (int i = 0; i<N; i++) {
-                String input = br.readLine();
-                String mid = input.substring(4, input.length()-4);
-                words[i] = new HashSet<>();
-                for (char c : mid.toCharArray()) {
-                    words[i].add(c);
-                }
-            }
-
-            chosen = new HashSet<>();
-            dfs(0, 0);
-            sb.append(max);
+        K = Integer.parseInt(st.nextToken());
+        if ( K < 5 ) {
+            System.out.println("0");
+            return;
         }
+        words = new int[N];
+        for (int i=0; i<N; i++) words[i] = wordToStatus(br.readLine());
+
+        for (int idx : learnedIdx) status = edit(status, idx, true);
+
+        // ******************** 메인 로직 ********************
+        dfs(0, 1);
 
 
+        // ******************** 정답 출력 ********************
+        sb.append(maxReadableWord);
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+        br.close();
     }
 
-    private static void dfs(int cnt, int start) {
-//        System.out.printf("[IN] cnt : %d, start: %d\n", cnt, start);
-        // start ~ 'z' 까지의 알파벳 개수 : 26 - start
-        // 앞으로 선택해야 하는 알파벳 개수 : M-5 - cnt
-        // 남은 알파벳 개수가 부족하면 안된다.
-        if (21 - start < M-5 - cnt) return;
-        if (cnt == M-5) {
-            // 선택한 문자로 할 수 있는지 검사
-            int c = 0;
-            for (int i = 0; i<N; i++) {
-                // 각 단어별
-                boolean hasAll = true;
-                for (char j : words[i]) {
-                    if (!chosen.contains(j) && !base.contains(j)) {
-                        hasAll = false;
-                        break;
-                    }
-                }
-                if (hasAll) c++;
+    public static void dfs(int depth, int startIdx) { // startIdx 건드리기
+//        System.out.printf("[IN] depth : %d\n",depth);
+//        printStatus(status);
+        if (depth == K-5) {
+            int numOfReadableWord = 0;
+            for (int word : words) {
+                if (canRead(word)) numOfReadableWord++;
             }
-//            System.out.printf("chosen : %s, canRead: %d\n", chosen, c);
-            max = Math.max(max, c);
+            maxReadableWord = Math.max(maxReadableWord, numOfReadableWord);
             return;
         }
 
-        // 단어 선택 (조합)
-        for (int i = start; i<21; i++) {
-            char alphabet = allCharacter[i];
-            chosen.add(alphabet);
-            dfs(cnt+1, i+1);
-            chosen.remove(alphabet);
+        for (int i=startIdx; i<26; i++) {
+            if (learnedIdx.contains(i)) continue;
+            status = edit(status, i, true);
+            dfs(depth+1, i+1);
+            status = edit(status, i, false);
         }
     }
 
+    public static boolean canRead(int word) {
+        return (status & word) == word;
+    }
+
+    public static int wordToStatus(String word) {
+        int len = word.length();
+        int wordStatus = 0;
+        for (int i=0; i<len; i++) {
+            wordStatus = edit(wordStatus, word.charAt(i)-'a', true);
+        }
+        return wordStatus;
+    }
+
+    public static int edit(int status, int idx, boolean mode) {
+        int v = 1 << idx;
+        if (mode) status |= v;
+        else status &= ~v;
+        return status;
+    }
+
+    public static int get(int status, int idx) {
+        return (status >> idx) & 1;
+    }
+
+    public static void printStatus(int status) {
+        System.out.println("-- status --");
+        for (int i=0; i<26; i++) {
+            System.out.printf("%d ", get(status, i));
+        }
+        System.out.println();
+    }
 }

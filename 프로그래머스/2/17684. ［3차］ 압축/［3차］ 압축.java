18ac2a7ae22697367a5d897f11af368e35b05f55
JuHyun Kim <95271588+skydreamer21@users.noreply.github.com>
2025-09-15 22:16:48 +0900

    [level 2] Title: [3차] 압축, Time: 16.56 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+class Solution {
+    static final int NOT_FOUND = -1;
+    
+    static class Index {
+        Map<String, Integer> index;
+        int next;
+        
+        public Index() {
+            index = new HashMap<>();
+            next = 1;
+            for (int i=0; i<26; i++) {
+                index.put(String.valueOf((char)('A'+i)), next++);
+            }
+        }
+        
+        public int find(String str) {
+            if (index.containsKey(str)) {
+                return index.get(str);
+            }
+            return NOT_FOUND;
+        }
+        
+        public void add(String str) {
+            index.put(str, next++);
+        }
+        
+        public boolean contains(String str) {
+            return index.containsKey(str);
+        }
+        
+    }
+    
+    static class Zipper {
+        Index index;
+        String target;
+        List<Integer> result;
+        int now;
+        int len;
+        
+        public Zipper(String target) {
+            this.index = new Index();
+            this.target = target;
+            now = 0;
+            len = target.length();
+            result = new ArrayList<>();
+        }
+        
+        private int findPossibleMaxFrom(int start) {
+            for (int i=start; i<len; i++) {
+                String sub = target.substring(start, i+1);
+                if (!index.contains(sub)) {
+                    return i;
+                }
+            }
+            return len;
+        }
+        
+        private int zipFrom(int start) {
+            int end = findPossibleMaxFrom(start);
+            String part = target.substring(start, end);
+            result.add(index.find(part));
+            if (end < len) {
+                index.add(target.substring(start, end+1));
+            }
+            return end;
+        }
+        
+        public void zip() {
+            int now = 0;
+            while(now < len) {
+                now = zipFrom(now);
+            }
+        }
+        
+        public int[] getResult() {
+            return result.stream().mapToInt(Integer::intValue).toArray();
+        }
+        
+    }
+    public int[] solution(String msg) {
+        Zipper zipper = new Zipper(msg);
+        zipper.zip();
+        return zipper.getResult();
+    }
+}
